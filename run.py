a8713d78e8ef65e3a219721b4721838354a73758
kev-n14 <kevinfinn688@gmail.com>
2022-11-28 11:59:06 +0000

    fix indentation and small errors

--- run.py
+++ run.py
@@ -9,15 +9,15 @@ class Board:
     def __init__(self,size, num_ships,name, type):
 
         self.size = size
-        self.board = [["." for x in range(size)]] for y in range(size)]
+        self.board = [["." for x in range(size)] for y in range(size)]
         self.num_ships = num_ships
         self.name = name
         self.type = type
         self.guess = []
-        self.ships. =[]
+        self.ships =[]
 
     def print(self):
-        for row in sewlf.board:
+        for row in self.board:
             print(" ".join(row))
     
     def game():
@@ -35,7 +35,9 @@ class Board:
         print("-"*30)
         compBoard = Board(size, num_ships, 'computer', type = "COMPUTER")
         playerBoard = Board(size, num_ships, user_name, type= "PLAYER") 
-        
-        
 
+
+    game()
+        
+        
 

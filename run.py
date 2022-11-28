f4b5f800b96056ee52768d19f9dcc41c584a5cf2
kev-n14 <kevinfinn688@gmail.com>
2022-11-28 22:12:07 +0000

    add display function, fix console display and design

--- run.py
+++ run.py
@@ -5,8 +5,7 @@ class Board:
     """
 
     """
-
-    def __init__(self,size, num_ships,name, type):
+    def __init__( self,size, num_ships,name, type):
 
         self.size = size
         self.board = [["." for x in range(size)] for y in range(size)]
@@ -16,37 +15,52 @@ class Board:
         self.guess = []
         self.ships =[]
 
-    def print(self):
-        for row in self.board:
-            print(" ".join(row))
     
-    def guess(self,x,y):
+    #def guess(self,x,y):
     
+   
+
+    #def add_ship(self,x,y type = 'COMPUTER'):
     
-    def add_ship(self,x,y type = 'COMPUTER'):
+    #def random_point(size):
+   
+    #return randint(0, size -1)
     
-    def random_point(size):
+    #def populate_board():
     
-    def populate_board():
+    #def make_guess():
     
-    def make_guess():
-    
-    def play_game(compBoard,playerBoard):
+    #def play_game(compBoard,playerBoard):
     
     
     def game():
         """
 
         """
+
         size = 2
         num_ships= 2
         scores['COMPUTER'] = 0
         scores['PLAYER'] = 0
 
-        print("-"*30)
-        print(" BATTLESHIPS ")
-        print(f"Board size: {size}. number of ships{num_ships}")
-        print("-"*30)
+        print("-"*40)
+        print("      ***** BATTLESHIPS *****     ")
+        print("-"*40)
+        
+        print()
+        
+        for col_num in range(1):
+            print('  | A | B | C | D | E | F | G | H |')
+
+            for row_num in range(1,9):
+                print("  ","---"*11)
+                print(row_num,"|",  "  | "*8)
+                #print("%d|%s|" % (row_num, "|".join(row)))
+            
+        print()
+        print('-'*40)
+        print(f"Board size: {size}. number of ships {num_ships}")
+        print("-"*40)
         compBoard = Board(size, num_ships, 'computer', type = "COMPUTER")
         playerBoard = Board(size, num_ships, user_name, type= "PLAYER") 
 

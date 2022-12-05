280a29b36948f2500266711548314aea7cddf73c
kev-n14 <kevinfinn688@gmail.com>
2022-12-05 03:15:06 +0000

    add comments

--- README.md
+++ README.md
@@ -1,77 +1,111 @@
-# Battleship Warzone Game
+#  Battleship Warzone Game
 Battleship Warzone is a Python terminal game, which runs in the Code Institute mock terminal on Heroku.
 
-The User has to try and beat the Computer by finding and destorying all 5 of the Computers boats before the Comptuer destorys their ships. The boats take up diferrent sizes ranging from 2 to 4
+The User has to try and beat the Computer by finding and destroying all 5 of the Computer's boats before the Computer destroys their ships. The boats take up different sizes ranging from 2 to 4
 
 [Click Here, To see a live version of my project.](https://battleship-kf.herokuapp.com/)
 ![Site on all Screen Sizes](images/on_all_screens.png)
 ## How To Play
 -----
-Battleship Warzone is based on the board game Battleship. You can learn more check here on [Wikipedia](https://en.wikipedia.org/wiki/Battleship_(game)). This version, the player competes against the computer. The user is first invited to place their ships on the board. The player has the oppertunity to place each boat either horizontally of verticle. Then the player is asked on what row and column they would like to place the ship. This process is repeated for each of the 5 ships.
+Battleship Warzone is based on the board game Battleship. You can learn more check here on [Wikipedia](https://en.wikipedia.org/wiki/Battleship_(game)). In this version, the player competes against the computer. The user is first invited to place their ships on the board. The player has the opportunity to place each boat either horizontally or verticle. Then the player is asked on what row and column they would like to place the ship. This process is repeated for each of the 5 ships.
 
-After each ship is placed on the board. The board is displayed with the updated position of the ship the user has just added. The ships are represented as collection of "O". The Player is asked to make their move to try and find the computers ships by ebtering the row and column they would like to select. the ships on the Computers board is hidden from the Player until they hit a ship by correctly guessing the position of a ship. 
+After each ship is placed on the board. The board is displayed with the updated position of the ship the user has just added. The ships are represented as a collection of "O". The Player is asked to make their move to try and find the computer's ships by ebtering the row and column they would like to select. the ships on the Computer board are hidden from the Player until they hit a ship by correctly guessing the position of a ship. 
 
-When a player hits the a ship this is represented by "X". A miss is represented by "-". The Player and Computer take it in turns to make guesses to try and find and sink each other's ships.
+When a player hits a ship this is represented by "X". A miss is represented by "-". The Player and Computer take turns to make guesses to try and find and sink each other's ships.
 The winner is the player who sinks all of their opponent's battleships first.
 ## Features
 -----
 ### Existing Features
 * Random board generation:
-    * the computers ships are randomly placed on the board.
-    * The Player can not see where the Comptuer's ships are.
-* Accepts input from Player
-* allows player to input name.
+    * The computer's ships are randomly placed on the board.
+    * The Player can not see where the Computer's ships are.
+
+![Computer random ships](images/computer_random_generator.png)
+
+* Accepts input from the Player
+* allows the player to input a name.
+
+![Computer random ships](images/display_input.png)
+
 * Allows the player to position ships on board.
+
+![Player populated board](images/user_populated_board.png)
+
 * Play against Computer
-* Ships are differnt sizes.
-* Keep track of scores of both Player and Computer.
+* Ships are different sizes.
+* Keep track of the scores of both Player and the Computer.
+
+![Player populated board](images/keep_score.png)
+
 * Input validation and error-handling
     * player cannot enter coordinates outside the size of the board
     * For the row user must enter numbers.
-    * For column user must enter a letters.
-    * User/computer cannot select same coordinates twice.
+    * For the column user must enter a letter.
+    * User/computer cannot select the same coordinates twice.
+
+![Player populated board](images/error_handling.png)
 
 ### Future Features
-* Add message to display when the player hit a ship
-* Improve graphics for user interface
-* add option for multiplayer
+* Add a message to display when the player hit a ship
+* Improve graphics for the user interface
+* add an option for multiplayer
+
+## Screenshots of GamePlay
+#### Display during the game Player's board
+![player board with hit and miss](images/gameplay1.png)
+
+#### Display of computers board during a game
+![Computers board hit and miss](images/gameplay2.png)
+
+#### Feedback to the user on a Miss
+![feedback to player](images/gameplay4.png)
+
+#### feedback to the user on Hit
+![correct hit](images/gameplay5.png)
+
+#### End of game message
+![end of game message](images/gameplay3.png)
+
+
 ## Data Model
 -----
- I decided to declare globally four Boards, Two for the player and two for the Computer, each have a board that stores the location of their ships(e.g USER_BOARD and PC_BOARD) and another board to store their moves made (e.g USER_MOVE_BOARD and PC_MOVE_BOARD)
+ I decided to declare globally four Boards, Two for the player and two for the Computer, each having a board that stores the location of their ships(e.g USER_BOARD and PC_BOARD) and another board to stores their moves made (e.g USER_MOVE_BOARD and PC_MOVE_BOARD)
 
- The Board's have a fixed size i.e 8 x 8 ,and number of ships i.e 5.
+ The Board has a fixed size i.e 8 x 8, and the number of ships i.e 5.
 
- The program has many functions to enable the game to run, such as game() that when called displays the Battleship logo, user board and calls other function to enable the player to populate the board.
+ The program has many functions to enable the game to run, such as game() that when called displays the Battleship logo, and Player board and calls other functions to enable the player to populate the board.
+
 ## Testing
 -----
 I have manually tested this project by :
-* passed the code through PEP8 Code Institute validator - no major errors were detected
+* passed the code through the PEP8 Code Institute validator - no major errors were detected
     * A few minor errors.
-* Passing it invalid values that such as strings when numbers are expected, numbers when a string is expected.
-* Positing the ships out of the parameters of the board.
+* Passing invalid values such as strings when numbers are expected, numbers when a string is expected.
+* Positioning the ships out of the parameters of the board.
 * Placing two ships in the same location.
 * Testing it in my local terminal and the Code Institute Heroku terminal.
 
+### Validator Testing
+* [PEP8 Code Institute validator:] (https://pep8ci.herokuapp.com/#)
+    * A few minor errors:
+        * 10 - for an invalid sequence for the Battleship logo (e.g invalid escape sequence )
+        * 11 - Line too long error
+
 ### Bugs
 #### Solved Bugs
-* The Player could enter the same cordinates muliple times and the program would accept this as a turn for the player. I added an if else statement to check to see if the place on the board was equal to "-" or "X". if so the player is asked to take their go again
+* The Player could enter the same coordinates multiple times and the program would accept this as a turn for the player. I added an if else statement to check to see if the place on the board was equal to "-" or "X". if so the player is asked to take their go again
 
 #### Remaining Bugs
 * As of the latest version of this project no known bugs have been found.
-
-### Validator Testing
-* [PEP8 Code Institute validator:] (https://pep8ci.herokuapp.com/#)
-    * A few minor errors for invalid sequence for the Battleship logo
-
 
 ## Deployment
 -----
 The Project was deployed using Code Institute's mock terminal for Heroku.
 * Steps to be taken for deployment:
-    * Fork or clone this respository
+    * Fork or clone this repository
     * Then click on <mark>Create new app</mark> button
     ![Create new app](images/deploy_step1.png)
-    * Choose a unique name for app and your region
+    * Choose a unique name for the app and your region
     * Click <mark>Create app</mark> button
     * On the ribbon tab go to <mark>Settings</mark> tab
     ![Settings](images/deploy_step2.png)
@@ -82,16 +116,14 @@ The Project was deployed using Code Institute's mock terminal for Heroku.
     ![Buildpacks](images/deploy_step4.png)
     * Go to <mark>Deploy</mark> section 
     * Scroll down to <mark>Deployment Method</mark> section -> Choose your preferred deployment method
-    * Then search for respository from the <mark>Connect to</mark> section and then click connect 
+    * Then search for repository from the <mark>Connect to</mark> section and then click connect 
     * Choose either Enable Automatic Deploys is on or off
-    * Choose branch to deploy in Manual deploy section 
+    * Choose the branch to deploy in the Manual deploy section 
     * Then click <mark>Deploy</mark> Button
     ![Deploy](images/deploy_step5.png)
-
-
 
 ## Credits
 -----
 * Code Institute for the deployment terminal and template.
 * Wikipedia for details of the Battleships game
-* 
+* geeksforgeeks - for research purposes
--- run.py
+++ run.py
@@ -10,9 +10,14 @@ LETTERS_TO_NUM = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7
 
 
 def print_board(board):
-    print(" ", "-"*16)
+    """
+    To print board with A to H for the columns
+    and row 1 to 8. For all boards passed in
+
+    """
+    print(" ", "-"* 16)
     print(' |A|B|C|D|E|F|G|H|')
-    print(" ", "-"*16)
+    print(" ", "-"* 16)
     row_number = 1
     for row in board:
         print("%d|%s|" % (row_number, "|".join(row)))
@@ -21,13 +26,16 @@ def print_board(board):
 
 
 def location_ships(board, name):
-    for ship_size in NUMBER_OF_SHIPS:
+    """
+
+    """
+    for ship_size in NUMBER_OF_SHIPS: # for loop to iterate through list of NUMBER_OF_SHIPS
         while True:
-            if board == PC_BOARD:
+            if board == PC_BOARD: # when board passed is the PC board
                 position, row, column = random.choice(
-                    ["H", "V"]), random.randint(0, 7), random.randint(0, 7)
-                if does_ship_fit(ship_size, row, column, position):
-                    if ship_extends_limit(board, row, column, position, ship_size) is False:
+                    ["H", "V"]), random.randint(0, 7), random.randint(0, 7)# random assign values for orientation, row, column
+                if does_ship_fit(ship_size, row, column, position): # passes values to function to determine if true
+                    if overlapping(board, row, column, position, ship_size) is False: # 
                         if position == "H":
                             for i in range(column, column + ship_size):
                                 board[row][i] = "O"
@@ -37,10 +45,10 @@ def location_ships(board, name):
                         break
             else:
                 location_ship = True
-                print('Place the ship with a length of ' + str(ship_size))
-                row, column, position = user_input(location_ship, name)
-                if does_ship_fit(ship_size, row, column, position):
-                    if ship_extends_limit(board, row, column, position, ship_size) is False:
+                print('Place the ship with a length of ' + str(ship_size))# message to player about size of ship
+                row, column, position = user_input(location_ship, name)# get values from player for row, column, orientation
+                if does_ship_fit(ship_size, row, column, position):# passes values to function to determine if true
+                    if overlapping(board, row, column, position, ship_size) is False: 
                         if position == "H":
                             for i in range(column, column + ship_size):
                                 board[row][i] = "O"
@@ -52,11 +60,19 @@ def location_ships(board, name):
 
 
 def user_name():
-    user_name = input("Please Enter Name: \n")
-    return user_name
+    """
+    Get Player name from user.
+    """
+    user_name = input("Please Enter Name: \n") # gets Players name
+    return user_name # returns Players name
 
 
 def user_input(location_ship, name):
+    """
+    To ask user orientation and position 
+    of each ship.
+    Ask user where to guess on computer board
+    """
     if location_ship is True:
         while True:
             try:
@@ -85,7 +101,7 @@ def user_input(location_ship, name):
     else:
         while True:
             try:
-                row = input("Enter the row 1-8 of the ship: \n")
+                row = input("Enter the row number (1-8) to attack : \n")
                 if row in '12345678':
                     row = int(row) - 1
                     break
@@ -93,7 +109,7 @@ def user_input(location_ship, name):
                 print('Enter a valid number between 1-8')
         while True:
             try:
-                column = input("Enter the column of the ship: \n").upper()
+                column = input("Enter the column letter (A-H) to attack: \n").upper()
                 if column in 'ABCDEFGH':
                     column = LETTERS_TO_NUM[column]
                     break
@@ -103,8 +119,12 @@ def user_input(location_ship, name):
 
 
 def does_ship_fit(NUMBER_OF_SHIPS, row, column, postion):
+    """
+    Determine if the ship will fit on board by it's
+    orientation, column letter and row number
+    """
     if postion == "H":
-        if column + NUMBER_OF_SHIPS > 8:
+        if column + NUMBER_OF_SHIPS > 8:  
             return False
         else:
             return True
@@ -115,7 +135,11 @@ def does_ship_fit(NUMBER_OF_SHIPS, row, column, postion):
             return True
 
 
-def ship_extends_limit(board, row, column, postion, ship_size):
+def overlapping(board, row, column, postion, ship_size):
+    """
+    To find out if the ship to be placed will overlap an existing ship
+    already in place.
+    """
     if postion == "H":
         for i in range(column, column + ship_size):
             if board[row][i] == "O":
@@ -128,6 +152,11 @@ def ship_extends_limit(board, row, column, postion, ship_size):
 
 
 def hit_counter(board):
+    """
+    Count each X or hit on a board.
+    return this number
+
+    """
     count = 0
     for row in board:
         for column in row:
@@ -137,40 +166,45 @@ def hit_counter(board):
 
 
 def move(board, name):
-    if board == USER_MOVE_BOARD:
-        row, column = user_input(USER_MOVE_BOARD, name)
-        if board[row][column] == "-":
+    """
+    to determine if the coordinates from the player or computer has already 
+    guessed if so tery again. if not, are these cordinates a  hit or miss. 
+
+    """
+    if board == USER_MOVE_BOARD: # when board is player move board
+        row, column = user_input(USER_MOVE_BOARD, name) # call function to print out questions to player, assign row, column
+        if board[row][column] == "-": # if coordinates passed have "-" assigned, then display message
             print("You already tried those coordinates. Try Again")
-            move(board, name)
-        elif board[row][column] == "X":
+            move(board, name) # function calls itself so player can guess again
+        elif board[row][column] == "X":# if coordinates passed have "X" assigned
             print("You already tried those coordinates. Try Again")
-            move(board, name)
-            
-        elif PC_BOARD[row][column] == "O":
-            board[row][column] = "X"
+            move(board, name)# function calls itself so player can guess again
+        elif PC_BOARD[row][column] == "O": # if coordinates on PC_Board are equal to "O"
+            board[row][column] = "X" # then mark board with "X" for a hit, then display message
             print(f"\n****** THAT WAS A HIT {name} *******\n")
 
         else:
-            board[row][column] = "-"
+            board[row][column] = "-" # mark board with "-" for a miss,then display message
             print(f"\n****** You MISSED {name} *******\n")
 
     elif board == PC_MOVE_BOARD:
-        row, column = random.randint(0, 7), random.randint(0, 7)
-        if board[row][column] == "-":
-            move(board, name)
-        elif board[row][column] == "X":
-            move(board, name)
-        elif USER_BOARD[row][column] == "O":
-            board[row][column] = "X"
+        row, column = random.randint(0, 7), random.randint(0, 7)# randomly assign values to row, column
+        if board[row][column] == "-":# if coordinates passed have "-" assigned
+            move(board, name)# function calls itself so computer can guess again
+        elif board[row][column] == "X":# if coordinates passed have "X" assigned
+            move(board, name)# function calls itself so computer can guess again
+        elif USER_BOARD[row][column] == "O":# if coordinates on USER_BOARD are equal to "O"
+            board[row][column] = "X"# then mark board with "X" for a hit, then display message
             print("***** The Computer Got A Hit ******")
         else:
-            board[row][column] = "-"
+            board[row][column] = "-" # mark board with "-" for a miss,then display message
             print("\n****** The Computer MISSED *******\n")
 
 
 def game():
     """
-
+    Starts a new game. Displays logo and calls functiions for user and computer to populate boards.
+    Displays text and calls functions for each round.  
     """
     print("-"*70)
     print("  ____       _______ _______ _      ______  _____ _    _ _____ _____  ")
@@ -181,17 +215,16 @@ def game():
     print(" |____/_/    \_\_|     |_|  |______|______|_____/|_|  |_|_____|_|")
     print(" ")
     print("-"*70)
-    name = user_name()
+    name = user_name()# player name is assinged to name 
     location_ships(PC_BOARD, name)
-    print_board(PC_BOARD)
+    #print_board(PC_BOARD) # to display the computers board (for testing purposes) 
     print(f"{name}\'s Board")
-    print_board(USER_BOARD)
-    location_ships(USER_BOARD, name)
-    count = 1
+    print_board(USER_BOARD) # players board  is printed
+    location_ships(USER_BOARD, name)# called to allow the user to populate board
+    round = 1 # value to count round
     while True:
         while True:
-            
-            print(f" ----- ROUND {count} -----")
+            print(f" ----- ROUND {round} -----")
             print(" ")
             hits_needed = sum(NUMBER_OF_SHIPS)
             print(f"{name} Score: {hit_counter(USER_MOVE_BOARD)}/{hits_needed} vs Computer Score: {hit_counter(PC_MOVE_BOARD)}/{hits_needed}")
@@ -200,7 +233,7 @@ def game():
             print_board(USER_MOVE_BOARD)
             move(USER_MOVE_BOARD, name)
             print(f"{name} score: {hit_counter(USER_MOVE_BOARD)} ")
-            count += 1
+            round += 1
             break
         if hit_counter(USER_MOVE_BOARD) == 17:
             print("*"*10, f"{name} IS THE WINNER ", "*"*10)
